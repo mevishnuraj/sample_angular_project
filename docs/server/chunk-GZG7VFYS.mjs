fbca7e594626ab8ec8f6de02dc47353520f83e54
Vishnuraj <rkvishnuraj@gmail.com>
2025-03-12 00:56:06 +0530

    live build

---
+++ docs/server/chunk-GZG7VFYS.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as p,b as r,c as i,h as n,k as m,m as a,n as f,q as s,w as c}from"./chunk-D67B5OUV.mjs";var o=class e{title="sampleproject";static \u0275fac=function(t){return new(t||e)};static \u0275cmp=p({type:e,selectors:[["app-root"]],standalone:!0,features:[i],decls:1,vars:0,template:function(t,A){t&1&&r(0,`Hello live again
+`)}})};var l=[];var g={providers:[n({eventCoalescing:!0}),c(l),f()]};var d={providers:[s()]},u=m(g,d);var v=()=>a(o,u),k=v;export{k as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GZG7VFYS.mjs";import"./chunk-D67B5OUV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
